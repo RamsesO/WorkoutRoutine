7bb344562004beed22e5f1c595f98a01bbd3b916
ramseso <ordonez.ramses@gmail.com>
2018-05-30 19:05:23 -0700

    initial recyclerview test

--- app/src/main/java/com/ramsesordonez/workoutroutineapplication/Exercise.java
+++ app/src/main/java/com/ramsesordonez/workoutroutineapplication/Exercise.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+package com.ramsesordonez.workoutroutineapplication;
+
+import java.util.Date;
 
 public class Exercise {
+    private String exerciseId;
+    private String name;
+
+
+
+    public Exercise(String exerciseId, String name){
+        this.exerciseId = exerciseId;
+        this.name = name;
+    }
+
+    public String getExerciseId() {
+        return exerciseId;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- app/src/main/java/com/ramsesordonez/workoutroutineapplication/HomeActivity.java
+++ app/src/main/java/com/ramsesordonez/workoutroutineapplication/HomeActivity.java
@@ -2,12 +2,68 @@ package com.ramsesordonez.workoutroutineapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class HomeActivity extends AppCompatActivity {
+
+    private EditText workoutTitle;
+    private Button buttonAddWorkout;
+    private RecyclerView workoutRecyclerView;
+    private RecyclerView.Adapter wAdapter;
+    private RecyclerView.LayoutManager wLayoutManager;
+
+    private DatabaseReference databaseWorkouts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        databaseWorkouts = FirebaseDatabase.getInstance().getReference("workouts");
+
+        workoutTitle = findViewById(R.id.text_workout_title);
+        buttonAddWorkout = findViewById(R.id.button_add_workout);
+        workoutRecyclerView = findViewById(R.id.workout_recycler_view);
+
+        wLayoutManager = new LinearLayoutManager(this);
+        workoutRecyclerView.setLayoutManager(wLayoutManager);
+
+//        wAdapter = new WorkoutAdapter(databaseWorkouts);
+//        workoutRecyclerView.setAdapter(wAdapter);
+
+        buttonAddWorkout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                addWorkout();
+            }
+        });
+    }
+
+
+    private void addWorkout(){
+        String name = workoutTitle.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(name)){
+            String id = databaseWorkouts.push().getKey();
+            Exercise e = new Exercise(id, name);
+
+            databaseWorkouts.child(id).setValue(e);
+
+            Toast.makeText(this, "exercise added", Toast.LENGTH_LONG).show();
+
+        }
+        else{
+            Toast.makeText(this, "enter a name", Toast.LENGTH_LONG).show();
+        }
     }
 }
+
