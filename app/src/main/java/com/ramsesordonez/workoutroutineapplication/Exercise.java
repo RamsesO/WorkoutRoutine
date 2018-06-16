bc12e31db54983d10fcf4745478ec76384a02cb4
ramseso <ordonez.ramses@gmail.com>
2018-06-16 03:25:05 -0700

    firebase uses maps instead of lists

--- app/src/main/java/com/ramsesordonez/workoutroutineapplication/Exercise.java
+++ app/src/main/java/com/ramsesordonez/workoutroutineapplication/Exercise.java
@@ -2,46 +2,83 @@ package com.ramsesordonez.workoutroutineapplication;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class Exercise {
 
-    private String mName;
-    private String mId;
-    private List<ExerciseSet> setList;
+    private String name;
+    private String id;
+    private Map<String, ExerciseSet> setList;
 
     private class ExerciseSet {
         int repsDone;
         int weight;
         Date date;
 
+        public ExerciseSet() {}
+
         public ExerciseSet(int w) {
             this.weight = w;
             date = new Date();
         }
 
-        public void setRepsDone(int s) {
-            this.repsDone = s;
+        public int getRepsDone() {
+            return repsDone;
         }
 
+        public void setRepsDone(int repsDone) {
+            this.repsDone = repsDone;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
     }
 
+    public Exercise() {}
+
     public Exercise(String name, String id) {
-        this.mName = name;
-        this.mId = id;
-        setList = new ArrayList<>();
+        this.name = name;
+        this.id = id;
+        setList = new HashMap<>();
     }
 
     public String getName() {
-        return mName;
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getId() {
-        return mId;
+        return id;
     }
 
-    public List<ExerciseSet> getWorkoutList() {
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Map<String, ExerciseSet> getSetList() {
         return setList;
+    }
+
+    public void setSetList(Map<String, ExerciseSet> setList) {
+        this.setList = setList;
     }
 }
--- app/src/main/java/com/ramsesordonez/workoutroutineapplication/Workout.java
+++ app/src/main/java/com/ramsesordonez/workoutroutineapplication/Workout.java
@@ -2,12 +2,14 @@ package com.ramsesordonez.workoutroutineapplication;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Workout {
     private String workoutId;
     private String name;
-    private List<Exercise> exerciseList;
+    private Map<String, Exercise> exerciseList;
 
     public Workout() {
 
@@ -16,7 +18,7 @@ public class Workout {
     public Workout(String workoutId, String name){
         this.workoutId = workoutId;
         this.name = name;
-        this.exerciseList = new ArrayList<>();
+        this.exerciseList = new HashMap<>();
     }
 
     public String getWorkoutId() {
@@ -35,11 +37,11 @@ public class Workout {
         this.name = name;
     }
 
-    public List<Exercise> getExerciseList() {
+    public Map<String, Exercise> getExerciseList() {
         return exerciseList;
     }
 
-    public void setExerciseList(List<Exercise> exerciseList) {
+    public void setExerciseList(Map<String, Exercise> exerciseList) {
         this.exerciseList = exerciseList;
     }
 }
